d265da9ef9e70e0189b1eeadc8b0fcf211e18d2a
Honepa <cot1884@gmail.com>
2020-09-08 20:19:47 +0500

    codemirror

---
+++ rmt_web/static/script_python.js
@@ -0,0 +1,8 @@
+var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+  mode: {name: "python",
+         version: 3,
+         singleLineStringErrors: false},
+  lineNumbers: true,
+  indentUnit: 4,
+  matchBrackets: true
+});
